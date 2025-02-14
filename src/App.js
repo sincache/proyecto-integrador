36fe7cbc846964c6c062669d9a8c37e09a4d8033
Emiliano Flores <jemilianofl@gmail.com>
2025-02-13 20:06:09 -0600

    Fix: Se corrigieron las rutas de los componentes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles.css'; // Estilos generales de bootstrap
 
-
-
 // Se deben importar los componentes de cada pagina
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home';     // Componente de la página de inicio
 // import About from './components/About';   // Componente "Acerca de", este componente se separo en 3 componentes para modificacion individual
-import QuienesSomos from './components/QuienesSomos';
-import MisionVisionValores from './components/MisionVisionValores';
-import TarjetaPresentacion from './components/TarjetasPresentacion';
+import QuienesSomos from './components/Quienes_Somos/QuienesSomos';
+import MisionVisionValores from './components/Quienes_Somos/MisionVisionValores';
+import TarjetaPresentacion from './components/Quienes_Somos/TarjetasPresentacion';
 
 function HomePage() {
   return (
