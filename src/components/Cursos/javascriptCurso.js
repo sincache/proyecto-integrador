d88e2909a2851861443e5c6ecf8ea62b1d02dbd1
Ale Olvera <vaolvera99@gmail.com>
2025-02-24 18:11:42 -0600

    added: alertas con bootstrap

--- src/components/Cursos/javascriptCurso.js
+++ src/components/Cursos/javascriptCurso.js
@@ -3,33 +3,30 @@ import React from "react";
 const JavaScriptCurso = () => {
     return (
         <div className="javascript-curso">
-            <main className="Contenido del curso">
-                <h1>Curso de Java: desde cero a experto.</h1>
-                <section className="introduccion-curso">
-                    <h2>Domina el JavaScript moderno.</h2>
-                    <p>Aprenda JavaScript desde los conceptos básicos hasta los conceptos avanzados con proyectos prácticos.</p>
+            <main className="contenido-curso">
+                <h1>Curso de JavaScript: desde cero a experto.</h1>
+                <section className="introduccion">
+                    <h2>Domina JavaScript moderno.</h2>
+                    <p>Aprende JavaScript desde los conceptos básicos hasta los más avanzados con proyectos prácticos.</p>
                 </section>
 
-                <section className="caracteristicas-curso">
+                <section className="caracteristicas">
                     <div className="caracteristica-card">
                         <h3>Aspectos destacados del curso:</h3>
                         <ul>
                             <li>Características de ES6+.</li>
                             <li>Programación asincrónica.</li>
                             <li>Manipulación del DOM.</li>
-                            <li>Marcos de trabajo agiles.</li>
+                            <li>Frameworks modernos.</li>
                         </ul>
                     </div>
 
                     <div className="caracteristica-card">
                         <h3>Lo que aprenderás:</h3>
                         <ul>
-                            <li>Variables.</li>
-                            <li>Tipos de datos.</li>
-                            <li>Funciones.</li>
-                            <li>Objetos.</li>
-                            <li>Arrays.</li>
-                            <li>Loops.</li>
+                            <li>Variables y tipos de datos.</li>
+                            <li>Funciones y objetos.</li>
+                            <li>Arrays y loops.</li>
                             <li>Manejo de errores.</li>
                         </ul>
                     </div>
