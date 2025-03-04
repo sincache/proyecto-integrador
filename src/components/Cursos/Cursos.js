982993f635b50ecf6ae20005f5db0ac0bee287ab
Alejandra Ordaz <aleog36@gmail.com>
2025-03-03 19:55:32 -0600

    resolved: conflicto en cursos.js

--- src/components/Cursos/Cursos.js
+++ src/components/Cursos/Cursos.js
@@ -82,7 +82,7 @@ const Cursos = () => {
                       <li><strong>Plataforma:</strong> {curso.PLATAFORMA}</li>
                       <li><strong>Duración:</strong> {curso.TIEMPO}</li>
                       <li><strong>Nivel:</strong> {curso.NIVEL}</li>
-                      <li><strong>Precio:</strong> {curso.PRECIO === 0 ? 'Gratis' : `$${curso.PRECIO}`}</li>
+                      <li><strong>Precio:</strong> {curso.PRECIO === 0 ? 'Gratis' : `${curso.PRECIO || "Gratuito"}`}</li>
                       <li>
                         <strong>Opiniones:</strong>
                         <span 
