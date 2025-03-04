828d8a9debfb7a1fee49c58ecf60039bd52fb8a4
Eddy <edgar.vilchis@politicas.unam.mx>
2025-03-03 21:32:06 -0600

    feat: Diseño iniciar sesion

--- src/components/Iniciar/Iniciar.jsx
+++ src/components/Iniciar/Iniciar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Link, useNavigate ,useLocation} from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import usersData from '../../assets/usuarios.json'
 ////Importar el json
 
 //import bcrypt from 'bcryptjs';
 
 
-const Iniciar =({setUser})=>{
+const Iniciar = ({ setUser }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -15,48 +15,48 @@ const Iniciar =({setUser})=>{
     const handleSubmit = (e) => {
         e.preventDefault();
         const userData = usersData.find((u) => u.username === username);
-        if(!userData){
+        if (!userData) {
             setError('Usuario no encontrado');
-            return; 
+            return;
         }
-    
-        
+
+
         const passwordMatch = userData.password === password;
-        if(passwordMatch){
+        if (passwordMatch) {
             setUser(userData.username);
             localStorage.setItem('user', userData.username);
             setError('');
             navigate('/Cursos');
-        }else{
+        } else {
             setError('Contraseña incorrecta');
         }
     };
 
 
-    return(
+    return (
 
-        <div className='d-flex vh-50 vw-50 aling-items-center justify-content-center min-h-screen bg-gray-200'>
-            <form onSubmit={handleSubmit} className='bg-black p-4 rounded shadow-lg w-100' style={{maxWidth: "600px"}}>
+        <div className='d-flex vh-100 vw-100 align-items-center justify-content-center bg-gray-200' id='Login'>
+            <form onSubmit={handleSubmit} className='bg-black p-4 rounded shadow-lg w-100' style={{ maxWidth: "600px" }}>
                 <h2 className='text-center text-white mb-4'>
                     Iniciar sesión
-                </h2> 
+                </h2>
                 <div className='mb-3'>
-                <input type='text'placeholder='Usuario' value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" required />  
+                    <input type='text' placeholder='Usuario' value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" required />
                 </div>
                 <div className='mb-3'>
-                <input type='password' placeholder='Contraseña' value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" required/>
+                    <input type='password' placeholder='Contraseña' value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" required />
                 </div>
-                
-                {error && <p className='text-red-500 text-sm mb-2'>{error}</p>} 
-                <button 
-                    type='submit' 
+
+                {error && <p className='text-red-500 text-sm mb-2'>{error}</p>}
+                <button
+                    type='submit'
                     className='btn btn-primary w-100'
-                > 
-                    Ingresar 
+                >
+                    Ingresar
                 </button>
             </form>
-            
         </div>
+
         
     );
 }
