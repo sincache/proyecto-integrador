1948b82ce8b1e20244dfcd030534a4f21d512bf2
Ale Olvera <vaolvera99@gmail.com>
2025-03-12 00:16:23 -0600

    feat:diseño cita

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,16 +6,27 @@ import VideoTWA from '../../assets/Video/VideoTWA.mp4';
 const Home = () => {
     return (
         <>
-    
-        <section className="hero">
+
+            <section className="hero">
                 <video id="VideoTWA" src={VideoTWA} autoPlay loop muted playsInline className="video-background"></video>
 
-        </section>
+            </section>
 
-            <div id="cita">
-                <p class="cita-home">"Las mujeres representan sólo el 26% de todos los puestos informáticos, y en el caso de las mujeres negras y latinas, las estadísticas son aún peores: combinadas, representan aproximadamente el 5% de todos los puestos informáticos."</p>
-                <p class="cita-dato">"Foro Económico Mundial (2023)"</p>
+            <div id="cita" className="cita-container">
+                <div className="cita-content">
+                    <p className="cita-text">
+                        Las mujeres representan sólo el
+                        <span className="dato-clave"> 26%</span> de todos los puestos informáticos, y en el caso de las mujeres negras y latinas, las estadísticas son aún peores: combinadas, representan aproximadamente el
+                        <span className="dato-clave"> 5%</span> de todos los puestos informáticos.
+                    </p>
+                    <p className="cita-dato">
+                        Foro Económico Mundial (2023)
+                    </p>
+                </div>
             </div>
+
+
+
             <div class="container row-2 align-items-center">
                 <section class="hero-content col-md-6">
                     <h1>
@@ -35,73 +46,73 @@ const Home = () => {
                 </section>
             </div>
 
-        
-        <main>
-            <section class="features">
-                <div class="container">
-                    <h2>
-                        Cursos diseñados para inspirar, empoderar y transformar.
-                    </h2>
-                    <div class="features-grid">
-                    <article class="feature-card">
-                        <h3>Talleres Interactivos</h3>
-                        <p>Talleres interactivos que fomentan el aprendizaje práctico y la colaboración.</p><br/>
-                        <a href="#" class="link-arrow">Inscríbete</a>
-                    </article>
-                    <article class="feature-card">
-                        <h3>Contenido Exclusivo</h3>
-                        <p>Accede a contenido exclusivo y recursos de aprendizaje de alta calidad.</p><br/>
-                        <a href="#" class="link-arrow">Únete</a>
-                    </article>
-                    <article class="feature-card">
-                        <h3>Mentoría Personalizada</h3>
-                        <p>Mentoría personalizada para guiarte en tu camino profesional en tecnología.</p><br/>
-                        <a href="#" class="link-arrow">Comenzar</a>
-                    </article>
-                </div>
-                </div>
-            </section>
-            
-            <section class="benefits">
-                <div class="container">
-                    <div class="benefits-content">
-                        <h2>Descubre los beneficios de unirte a TechWomen Academy hoy mismo</h2>
-                        <ul class="benefits-list">
-                            <li>Acceso a mentores con experiencia en la industria</li>
-                            <li>Aprendizaje flexible que se adapta a tu horario</li>
-                            <li>Una comunidad de apoyo para mujeres en tecnología</li>
-                            <li>Contenido mensual</li>
-                        </ul>
+
+            <main>
+                <section class="features">
+                    <div class="container">
+                        <h2>
+                            Cursos diseñados para inspirar, empoderar y transformar.
+                        </h2>
+                        <div class="features-grid">
+                            <article class="feature-card">
+                                <h3>Talleres Interactivos</h3>
+                                <p>Talleres interactivos que fomentan el aprendizaje práctico y la colaboración.</p><br />
+                                <a href="#" class="link-arrow">Inscríbete</a>
+                            </article>
+                            <article class="feature-card">
+                                <h3>Contenido Exclusivo</h3>
+                                <p>Accede a contenido exclusivo y recursos de aprendizaje de alta calidad.</p><br />
+                                <a href="#" class="link-arrow">Únete</a>
+                            </article>
+                            <article class="feature-card">
+                                <h3>Mentoría Personalizada</h3>
+                                <p>Mentoría personalizada para guiarte en tu camino profesional en tecnología.</p><br />
+                                <a href="#" class="link-arrow">Comenzar</a>
+                            </article>
+                        </div>
                     </div>
-                    <div class="logo-container">
-                        <img id="logo-body" src={LogoTWA} alt="TechWomen Academy Logo" class="logo" />
+                </section>
+
+                <section class="benefits">
+                    <div class="container">
+                        <div class="benefits-content">
+                            <h2>Descubre los beneficios de unirte a TechWomen Academy hoy mismo</h2>
+                            <ul class="benefits-list">
+                                <li>Acceso a mentores con experiencia en la industria</li>
+                                <li>Aprendizaje flexible que se adapta a tu horario</li>
+                                <li>Una comunidad de apoyo para mujeres en tecnología</li>
+                                <li>Contenido mensual</li>
+                            </ul>
+                        </div>
+                        <div class="logo-container">
+                            <img id="logo-body" src={LogoTWA} alt="TechWomen Academy Logo" class="logo" />
+                        </div>
                     </div>
-                </div>
-            </section>
-            
-            <section class="enrollment">
-                <div class="container">
-                    <h2>¿Cómo inscribirte a los cursos y talleres?</h2>
-                    <div class="enrollment-grid">
-                        <article class="enrollment-step">
-                            <h3>Participación en Talleres y Certificaciones</h3>
-                            <p>Una vez inscrita, recibirás toda la información necesaria para comenzar.</p>
-                        </article>
-                        <article class="enrollment-step">
-                            <h3>Acceso a Materiales y Recursos</h3><br/>
-                            <p>Tendrás acceso a materiales exclusivos para tu aprendizaje.</p>
-                        </article>
-                        <article class="enrollment-step">
-                            <h3>Certificación al Completar el Curso</h3>
-                            <p>Al finalizar, recibirás una certificación que valida tus habilidades.</p>
-                        </article>
+                </section>
+
+                <section class="enrollment">
+                    <div class="container">
+                        <h2>¿Cómo inscribirte a los cursos y talleres?</h2>
+                        <div class="enrollment-grid">
+                            <article class="enrollment-step">
+                                <h3>Participación en Talleres y Certificaciones</h3>
+                                <p>Una vez inscrita, recibirás toda la información necesaria para comenzar.</p>
+                            </article>
+                            <article class="enrollment-step">
+                                <h3>Acceso a Materiales y Recursos</h3><br />
+                                <p>Tendrás acceso a materiales exclusivos para tu aprendizaje.</p>
+                            </article>
+                            <article class="enrollment-step">
+                                <h3>Certificación al Completar el Curso</h3>
+                                <p>Al finalizar, recibirás una certificación que valida tus habilidades.</p>
+                            </article>
+                        </div>
+                        <div class="cta-buttons">
+                            <a href="#" class="btn btn-primary">Saber Más</a>
+                        </div>
                     </div>
-                    <div class="cta-buttons">
-                        <a href="#" class="btn btn-primary">Saber Más</a>
-                    </div>
-                </div>
-            </section>
-        </main>
+                </section>
+            </main>
         </>
 
     );
